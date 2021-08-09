82ba0ac18a8a52e7b114c708b571e0dfcc2916b8
Spiros7bit <tsiouprosspiros@gmail.com>
2021-08-09 17:18:57 +0300

    Now a player cant enter X or O to other player's box

--- tictactoe.py
+++ tictactoe.py
@@ -1,17 +1,6 @@
-plaisio = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-]
-
-ROUNDS = 9
-turn = True #when turn==True player_x play
-player_x = "player1"
-player_o = "player2"
-rounds = 0 #counter of rounds
-
-
 def enter_this(row, col, plaisio):
+    #this function enter the inputs on the boxes (X or O)
+
     if turn:
         plaisio[row][col] = "X" 
         return 
@@ -19,7 +8,10 @@ def enter_this(row, col, plaisio):
     plaisio[row][col] = "O"
 #end of enter_this
 
+
 def display_plaisio(plaisio):
+    #this function display the game status
+
     for row in range( len(plaisio) ):
         print("\t\t +----------------------------------------------+")
         print("\n\t\t |\t",plaisio[row][0],"\t|\t" ,plaisio[row][1],"\t|\t", plaisio[row][2], "\t|\t\t\n")
@@ -27,37 +19,42 @@ def display_plaisio(plaisio):
     print("\t\t +----------------------------------------------+")
 #end of display_plaisio
 
+
 def check_winner(plaisio):
+    #this function check if there are 3 "O" on every list from the end_game function and return the right result
+
     r, l, x, s = end_game(plaisio)
 
     if 3 in r:
-        return False #player_o win
+        return 0 #player_o win
     elif 0 in r:
-        return True #player_x win
+        return 1 #player_x win
 
     if 3 in l:
-        return False #player_o win
+        return 0 #player_o win
     elif 0 in l:
-        return True #player_x win
+        return 1 #player_x win
     
     if x == 3:
-        return False #player_o win
+        return 0 #player_o win
     elif x == 0:
-        return True #player_x win
+        return 1 #player_x win
 
     if s == 3:
-        return False #player_o win
+        return 0 #player_o win
     elif s == 0:
-        return True #player_x win
+        return 1 #player_x win
     
 #end of check_winner
 
+
 def end_game(plaisio):
+    #store the "O" from every row, comlum on lists and the diagonals "O" on variables and return them
 
     r1 = 0
     r2 = 0
     r3 = 0
-    #rows
+    #store the number of "O" from every row on a list
     for i in range(3):
         if "O" in plaisio[0][i]: #0 row
             r1 += 1
@@ -72,7 +69,7 @@ def end_game(plaisio):
     l1 = 0
     l2 = 0
     l3 = 0        
-    #columns
+    #store the number of "O" from every comlumn on a list
     for i in range(3):
         if "O" in plaisio[i][0]: #0 column
             l1 += 1
@@ -85,89 +82,85 @@ def end_game(plaisio):
     l_list = [l1, l2, l3]
 
     x = 0
+    #store the number of "O" from diagonla on a variable
     for i in range(3):
-        if "O"in plaisio[i][i]:
-            x+=1
-        
         if "O" in plaisio[i][i]:
             x+=1
 
-        if "O" in plaisio[i][i]:
-            x+=1
-
+    #store the number of "O" from oposite diagonal on a variable
     s = 0
     if "O" in plaisio[0][2]:
         s+=1
-
     if "O" in plaisio[1][1]:
         s+=1
-
     if "O" in plaisio[2][0]:
         s+=1
 
     return r_list, l_list, x, s
 #end of end_game
-                      
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-###########################main program if __name__ = "__main__":
-
-player_o = input("Enter the name of player O: ")
-player_x = input("Enter the name of player X: ")
-
-
-while rounds < ROUNDS:
-    if turn :
-        print(player_x, " ,enter the X on a box------------------")
-
-        row = int( input("1. Enter the row: ") )
-        col = int( input("2. Enter the column: ") )
-            
-        while col >= 3 or row >= 3:
-            row = int( input("Wrong input, enter the row again: ") )
-            col = int( input("Wrong input, enter the column again: ") )
-
-        enter_this(row, col, plaisio)
-
-        turn = False
-
-    else:
-        print(player_o, " ,enter the O on a box------------------")
-
-        row = int( input("1. Enter the row: ") )
-        col = int( input("2. Enter the column: ") )
-            
-        while col >= 3 or row >= 3:
-            row = int( input("Wrong input, enter the row again: ") )
-            col = int( input("Wrong input, enter the column again: ") )
-
-        enter_this(row, col, plaisio)
-
-        turn = True
+#--------------------------------main 
+if __name__ == "__main__":
     
-    rounds+=1
-    display_plaisio(plaisio)
+    #attributes
+    plaisio = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+]
+
+    ROUNDS = 9
+    turn = True #when turn==True player_x play
+    rounds = 0 #counter of rounds
 
 
-win = check_winner(plaisio)
+    #players enter their names
+    player_o = input("Enter the name of player O: ")
+    player_x = input("Enter the name of player X: ")
 
-if win == False:
-    print("The winner is: ", player_o)
-elif win == True:
-    print("The winner is: ", player_x)
-else:
-    print("Draw...")
+    #there are 9 turns
+    while rounds < ROUNDS:
+        if turn: #playerX turn
+            print("\n",player_x, " ,enter the X on a box------------------")
+
+            row = int( input("1. Enter the row (0-2): ") )
+            col = int( input("2. Enter the column (0-2): ") )
+                
+            while (col >= 3 or row >= 3) or plaisio[row][col] != "":
+                row = int( input("Wrong input, enter the row again (0-2): ") )
+                col = int( input("Wrong input, enter the column again (0-2): ") )
+
+            enter_this(row, col, plaisio) #put the X on this box
+
+            turn = False
+
+        else: #playerO turn
+            print("\n",player_o, " ,enter the O on a box------------------")
+
+            row = int( input("1. Enter the row (0-2): ") )
+            col = int( input("2. Enter the column (0-2): ") )
+                
+            while (col >= 3 or row >= 3) or plaisio[row][col] != "":
+                row = int( input("Wrong input, enter the row again (0-2): ") )
+                col = int( input("Wrong input, enter the column again (0-2): ") )
+
+            enter_this(row, col, plaisio) #put the O on this box
+
+            turn = True
+        
+        rounds+=1
+        display_plaisio(plaisio)
+
+    win = 3 #initial draw
+    win = check_winner(plaisio) 
+
+    print("\n-----------------------")
+    if win == 0:
+        print("The winner is: ", player_o)
+    elif win == 1:
+        print("The winner is: ", player_x)
+    else:
+        print("Draw...")
 
 
